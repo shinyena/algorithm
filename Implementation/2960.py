56050261c3e317830e19b9e11827dd3fce682942
yenashin <yena5790@gachon.ac.kr>
2023-03-07 01:17:01 +0900

    Commit

---
+++ Implementation/2960.py
@@ -0,0 +1,26 @@
+# 성공
+# 백준 Sliver4
+# 메모리: 31256KB
+# 시간: 48ms
+
+n, k = map(int, input().split())
+numbers = []
+
+for i in range(2, n+1):
+    numbers.append(i)
+
+cnt = 0
+while cnt != k:
+    p = numbers.pop(0)
+    cnt += 1
+
+    for number in numbers:
+        if number%p == 0:
+            numbers.remove(number)
+            cnt += 1
+
+            if cnt == k:
+                p = number
+                break
+
+print(p)
