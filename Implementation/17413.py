304e96dc7239583c06dde14a042d9a41188a1d7c
yenashin <yena5790@gachon.ac.kr>
2023-03-22 16:57:47 +0900

    Commit

--- Implementation/17413.py
+++ Implementation/17413.py
@@ -1,30 +1,18 @@
 # 성공
 # 백준 Sliver3
-# 메모리: 32820KB
-# 시간: 104ms
+# 메모리: 31256KB
+# 시간: 44ms
 
+s = input().replace("<","A<").replace(">",">A")
+arr = s.split("A")
 
-s = input()
-
-tmp = []
-idx = 0
-while idx != len(s):
-    if s[idx] == " ":
-        if len(tmp) != 0:
-            print(''.join(str(t) for t in reversed(tmp)), end="")
-            tmp = []
-        print(" ", end="")
-    elif s[idx] == "<":
-        if len(tmp) != 0:
-            print(''.join(str(t) for t in reversed(tmp)), end="")
-            tmp = []
-        print(s[idx], end="")
-        while s[idx] != ">":
-            idx += 1
-            print(s[idx], end="")
+for a in arr:
+    if "<" in a and ">" in a:
+        print(a, end="")
     else:
-        tmp.append(s[idx])
-    idx += 1
-
-if len(tmp) != 0:
-    print(''.join(str(t) for t in reversed(tmp)), end="")
+        brr = a.split(" ")
+        for i in range(len(brr)):
+            if i != len(brr) - 1:
+                print(brr[i][::-1], end=" ")
+            else:
+                print(brr[i][::-1], end="")
