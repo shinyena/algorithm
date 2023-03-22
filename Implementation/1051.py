edb93957ea7718904675f7628d5a2b0973128809
yenashin <yena5790@gachon.ac.kr>
2023-03-22 16:05:25 +0900

    Commit

--- Implementation/1051.py
+++ Implementation/1051.py
@@ -1,32 +1,15 @@
 # 성공
 # 백준 silver4
-# 메모리: 31388KB
-# 시간: 1344ms
+# 메모리: 31256KB
+# 시간: 44ms
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().strip())) for _ in range(n)]
-visited = set()
-
-def find(brr):
-    max = 1
-    for i in range(len(brr)-3):
-        x = brr[i][0]
-        y = brr[i][1]
-        for d in range(min(n-x, m-y)):
-            if (x+d,y) in brr and (x,y+d) in brr and (x+d,y+d) in brr:
-                if (d+1)*(d+1) > max:
-                    max = (d+1)*(d+1)
-    return max
 
 
-answer = 1
-for x in range(10):
-    brr = []
-    for i in range(n):
-        for j in range(m):
-            if arr[i][j] == x:
-                brr.append((i, j))
-    if len(brr)>=4:
-        if find(brr)>answer:
-            answer = find(brr)
-print(answer)
+for x in range(min(n, m), -1, -1):
+    for i in range(n-x):
+        for j in range(m-x):
+            if arr[i][j] == arr[i][j+x] == arr[i+x][j] == arr[i+x][j+x]:
+                print((x+1)**2)
+                exit(0)
